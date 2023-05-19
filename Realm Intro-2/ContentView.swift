9c2741cdc07745041069615d7966a0abd7b06769
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-05-19 02:50:37 +0200

    first commit: Load initial files

---
+++ Realm Intro-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Realm Intro-2
+//
+//  Created by Stewart Lynch on 2022-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
