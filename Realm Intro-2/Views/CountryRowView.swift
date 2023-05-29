8d2eba22d51118679b62d950ce5419d405e2aa96
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-05-29 17:22:35 +0200

    feat: Flag Picker (Completed Realm+SwiftUI 2 One Many Relationship Part 2)

--- Realm Intro-2/Views/CountryRowView.swift
+++ Realm Intro-2/Views/CountryRowView.swift
@@ -11,8 +11,15 @@ import RealmSwift
 struct CountryRowView: View {
     @ObservedRealmObject var country: Country
     @FocusState var isFocused: Bool?
+    @State private var showFlagPicker = false
     var body: some View {
         HStack {
+            Button {
+                showFlagPicker = true
+            } label: {
+                Text(country.flag)
+            }
+            .buttonStyle(.plain)
             TextField("New Country", text: $country.name)
                 .focused($isFocused, equals: true)
                 .textFieldStyle(.roundedBorder)
@@ -21,6 +28,9 @@ struct CountryRowView: View {
         }
         .padding()
         .frame(height: 30)
+        .sheet(isPresented: $showFlagPicker) {
+            FlagPicker(country: country)
+        }
     }
 }
 
---
+++ Realm Intro-2/Views/FlagPicker.swift
@@ -0,0 +1,60 @@
+//
+//  FlagPicker.swift
+//  Realm Intro-2
+//
+//  Created by Marcin Jędrzejak on 29/05/2023.
+//
+
+import SwiftUI
+import RealmSwift
+
+struct FlagPicker: View {
+    @ObservedRealmObject var country: Country
+    @Environment(\.dismiss) var dismiss
+    @State private var searchText = ""
+    var searchResults: [CountryInfo] {
+        if searchText.isEmpty {
+            return CountryInfo.array
+        } else {
+            return CountryInfo.array.filter { $0.name.lowercased().contains(searchText.lowercased())}
+        }
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(searchResults) { countryInfo in
+                    Button {
+                        $country.flag.wrappedValue = countryInfo.flag
+                        dismiss()
+                    } label: {
+                        HStack {
+                            Text("\(countryInfo.name) \(countryInfo.flag)")
+                            Spacer()
+                            if country.flag == countryInfo.flag {
+                                Image(systemName: "checkmark")
+                            }
+                        }
+                    }
+                }
+            }
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+            .navigationTitle("Flags")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "x.circle")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct FlagPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        FlagPicker(country: Country(name: "Poland"))
+    }
+}
